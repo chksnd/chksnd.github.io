7b6e85943291c90f646a1e3807c0931b6521040a
aimazh <aimazh@pro.local>
2020-01-12 21:59:46 +0100

    Updates

---
+++ precache-manifest.527da13b7b7da0bce31f0f97e34600f8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ac951f9c762d75aacc50cadd5b9cf2",
+    "url": "/chksndapp/index.html"
+  },
+  {
+    "revision": "46908bffc4e9cde47994",
+    "url": "/chksndapp/static/css/main.7528947b.chunk.css"
+  },
+  {
+    "revision": "4fe528706c03a8e1c03e",
+    "url": "/chksndapp/static/js/2.e01fe886.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/chksndapp/static/js/2.e01fe886.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46908bffc4e9cde47994",
+    "url": "/chksndapp/static/js/main.837283d9.chunk.js"
+  },
+  {
+    "revision": "ee968966e888e0289d1c",
+    "url": "/chksndapp/static/js/runtime-main.4f978f23.js"
+  },
+  {
+    "revision": "6d49b9eb0e8e6ff05bd0e5046099f8f6",
+    "url": "/chksndapp/static/media/app-logo.6d49b9eb.png"
+  },
+  {
+    "revision": "c7733d8fe7ded2e88a8fba1d8e720f09",
+    "url": "/chksndapp/static/media/app-screen.c7733d8f.png"
+  },
+  {
+    "revision": "e7622b2be1e1041e9e398c864c2ed248",
+    "url": "/chksndapp/static/media/cover.e7622b2b.png"
+  }
+]);
