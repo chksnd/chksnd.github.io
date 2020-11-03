bd7b3358f061c53bca99b6fc6ee2232965ea1569
aimazh <aimazh@pro.local>
2020-11-03 22:03:51 +0100

    Updates

---
+++ precache-manifest.512a19e1b0ec85216aceae8cb9fa323b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e1063c71d79053e5f8ae5ec9463745",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33242bd557e3b3911149",
+    "url": "/static/css/main.cc37ec27.chunk.css"
+  },
+  {
+    "revision": "9706a039e3618a9c1fe8",
+    "url": "/static/js/2.c3810bc7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c3810bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33242bd557e3b3911149",
+    "url": "/static/js/main.ef48a645.chunk.js"
+  },
+  {
+    "revision": "c93fe328ab96a6b2efbe",
+    "url": "/static/js/runtime-main.bb5871b1.js"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/static/media/app-badge-android.db9b21a1.png"
+  },
+  {
+    "revision": "6d49b9eb0e8e6ff05bd0e5046099f8f6",
+    "url": "/static/media/app-logo.6d49b9eb.png"
+  },
+  {
+    "revision": "2ec13aae8edc813c4d388d88710eae68",
+    "url": "/static/media/app-screen.2ec13aae.png"
+  },
+  {
+    "revision": "e7622b2be1e1041e9e398c864c2ed248",
+    "url": "/static/media/cover.e7622b2b.png"
+  },
+  {
+    "revision": "d60e47b0db25886f24e6e08fcd02b509",
+    "url": "/static/media/promo.d60e47b0.webm"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chksndapp/precache-manifest.b22ffd650ad5bc3ddf27c16ae84fb30b.js"
+  "/precache-manifest.512a19e1b0ec85216aceae8cb9fa323b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chksndapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
