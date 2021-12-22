c55ff471eb4e05593e165e03c4497bc29df7e997
Aibek Mazhitov <amazhitov@gmail.com>
2021-12-22 01:02:46 +0100

    Updates

---
+++ precache-manifest.48563949fe6f04e30f0b4887bdb29867.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f421fd1ccfae2c9d9384601f41d7b86b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d6c51c2f328f130b5f3",
+    "url": "./static/css/main.142a80ce.chunk.css"
+  },
+  {
+    "revision": "cd17236947a022dda623",
+    "url": "./static/js/2.a5ea9dfd.chunk.js"
+  },
+  {
+    "revision": "5a03f2a20c2b2c3a29da5cc95f624952",
+    "url": "./static/js/2.a5ea9dfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d6c51c2f328f130b5f3",
+    "url": "./static/js/main.06f67fec.chunk.js"
+  },
+  {
+    "revision": "e58e29553655cca0daaf",
+    "url": "./static/js/runtime-main.a60f064b.js"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "./static/media/app-badge-android.db9b21a1.png"
+  },
+  {
+    "revision": "6d49b9eb0e8e6ff05bd0e5046099f8f6",
+    "url": "./static/media/app-logo.6d49b9eb.png"
+  },
+  {
+    "revision": "2ec13aae8edc813c4d388d88710eae68",
+    "url": "./static/media/app-screen.2ec13aae.png"
+  },
+  {
+    "revision": "d60e47b0db25886f24e6e08fcd02b509",
+    "url": "./static/media/promo.d60e47b0.webm"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ea2a3b7afd23c76e7d7762d05434348.js"
+  "./precache-manifest.48563949fe6f04e30f0b4887bdb29867.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
