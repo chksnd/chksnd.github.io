6d877093895e96675b3e993f133de72da286d18c
Aibek Mazhitov <amazhitov@gmail.com>
2021-12-22 01:37:43 +0100

    Updates

---
+++ precache-manifest.d13452ff5d0294791bdae27332c9ca23.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801581d7d3d203090c964069447f5532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ae89b65cc0423a06517",
+    "url": "/static/css/main.cfa170c2.chunk.css"
+  },
+  {
+    "revision": "aede480a4119e345c2dc",
+    "url": "/static/js/2.47939090.chunk.js"
+  },
+  {
+    "revision": "f9149ae0f0977793c838a92fcf8ae78b",
+    "url": "/static/js/2.47939090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae89b65cc0423a06517",
+    "url": "/static/js/main.b455c15f.chunk.js"
+  },
+  {
+    "revision": "c93fe328ab96a6b2efbe",
+    "url": "/static/js/runtime-main.bb5871b1.js"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/static/media/app-badge-android.db9b21a1.png"
+  },
+  {
+    "revision": "2ec13aae8edc813c4d388d88710eae68",
+    "url": "/static/media/app-screen.2ec13aae.png"
+  },
+  {
+    "revision": "c975e2d92e9c44d6bdcfef28345af292",
+    "url": "/static/media/logo.c975e2d9.svg"
+  },
+  {
+    "revision": "d60e47b0db25886f24e6e08fcd02b509",
+    "url": "/static/media/promo.d60e47b0.webm"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.48563949fe6f04e30f0b4887bdb29867.js"
+  "/precache-manifest.d13452ff5d0294791bdae27332c9ca23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
